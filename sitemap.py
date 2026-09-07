ac43d27118d71d94535cfcac79c332091dce866c
ruo-edwin <ruoedwin@gmail.com>
2026-09-07 12:16:47 +0300

    added

--- sitemap.py
+++ sitemap.py
@@ -27,7 +27,6 @@ async def sitemap():
     xml = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 """
-"""
 
     for url in urls:
         xml += f"""
